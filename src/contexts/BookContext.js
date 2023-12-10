aa5d41be1e7cd5540bad7e53acfbc67cec094ecb
deepak <deepamdeebu@gmail.com>
2023-12-10 17:43:34 +0530

    Context : Use functional context approach instead of class context for booklist component

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    {title: 'Book 1', id: 1},
+    {title: 'Book 2', id: 2},
+  ]);
+
+  return (
+    <BookContext.Provider value={{ books }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+
+export default BookContextProvider;
